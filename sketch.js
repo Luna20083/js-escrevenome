d189eb31abe733e27842426b54b196ed0a90d96f
Luna20083 <afonsomariapaula798@gmail.com>
2024-06-18 13:33:20 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -43,3 +43,23 @@ function setup() {
     
     console.log(mouseIsPressed);
     rect(mouseX, mouseY, 100, 150);
+  }
+  function setup() {
+    // cria uma tela de 400px de largura por 400px de altura
+    createCanvas(400, 400);
+  }
+  function setup() {
+    createCanvas(400, 400);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
